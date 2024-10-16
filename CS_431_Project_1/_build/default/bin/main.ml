11940781ac846832cbc4c49dbca8d268969146e2
Dylan Bartell <barteldf@sunypoly.edu>
2024-10-16 14:32:14 -0400

    Added multiprocess reduce function as well

--- CS_431_Project_1/_build/default/bin/main.ml
+++ CS_431_Project_1/_build/default/bin/main.ml
@@ -9,9 +9,22 @@ let multimap ar func =
   done;
   Riot.wait_pids !pids;;
 
+let multireduce ar sum =
+  let intermediate sum a =
+    sum := !sum + a in
+  let pids = ref [] in
+  for i = 0 to (Array.length ar) - 1 do
+    pids := (Riot.spawn (fun () -> intermediate sum ar.(i))) :: !pids
+  done;
+  Riot.wait_pids !pids;;
+
+
 Riot.run @@ fun () ->
   let ar = [|1;2|] in
   multimap ar addone;
+  let sum = ref 0 in
+  multireduce ar sum;
+  Format.printf "%d " !sum;
   for i = 0 to (Array.length ar) - 1 do
     Format.printf "%d " ar.(i);
   done;
