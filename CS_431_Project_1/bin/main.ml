936dce050b01b82dbd8e18b3df13168df7c91db8
Dylan Bartell <barteldf@sunypoly.edu>
2024-10-16 14:10:09 -0400

    Map function is now standalone; map function effectively complete

--- CS_431_Project_1/bin/main.ml
+++ CS_431_Project_1/bin/main.ml
@@ -1,15 +1,17 @@
 let addone x = x + 1;;
 
-let intermediate ar i func =
-  ar.(i) <- func ar.(i);;
+let multimap ar func =
+  let intermediate a i f =
+    a.(i) <- f a.(i) in
+  let pids = ref [] in
+  for i = 0 to (Array.length ar) - 1 do
+    pids := (Riot.spawn (fun () -> intermediate ar i func)) :: !pids
+  done;
+  Riot.wait_pids !pids;;
 
 Riot.run @@ fun () ->
   let ar = [|1;2|] in
-  let pids = ref [] in
-  for i = 0 to (Array.length ar) - 1 do
-    pids := (Riot.spawn (fun () -> intermediate ar i addone)) :: !pids
-  done;
-  Riot.wait_pids !pids;
+  multimap ar addone;
   for i = 0 to (Array.length ar) - 1 do
     Format.printf "%d " ar.(i);
   done;
