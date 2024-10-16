bc1bd419afc1a4bb2ade94825fa2a718dc716d8a
Dylan Bartell <barteldf@sunypoly.edu>
2024-10-16 13:19:28 -0400

    Map function works with multiprocessing (may need to be optimized)

--- CS_431_Project_1/_build/default/bin/main.ml
+++ CS_431_Project_1/_build/default/bin/main.ml
@@ -5,9 +5,11 @@ let intermediate ar i func =
 
 Riot.run @@ fun () ->
   let ar = [|1;2|] in
+  let pids = ref [] in
   for i = 0 to (Array.length ar) - 1 do
-    let () = Riot.spawn (fun () -> intermediate ar i addone);
+    pids := (Riot.spawn (fun () -> intermediate ar i addone)) :: !pids
   done;
+  Riot.wait_pids !pids;
   for i = 0 to (Array.length ar) - 1 do
     Format.printf "%d " ar.(i);
   done;
