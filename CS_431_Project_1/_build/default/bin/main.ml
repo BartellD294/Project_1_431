c8108c4a7f9df990bd71d34f04cbac64d2eeffff
Noah Breedy <breedyn@sunypoly.edu>
2024-10-27 16:32:59 -0400

    Cleaned up dune build and added multiFilter fucntion

--- CS_431_Project_1/_build/default/bin/main.ml
+++ CS_431_Project_1/_build/default/bin/main.ml
@@ -1,31 +1,68 @@
+(* Increment function *)
 let addone x = x + 1;;
 
-let multimap ar func =
-  let intermediate a i f =
+(* Parallel map function *)
+let multimap ls func =
+  let ar = Array.of_list ls in
+  let apply_func a i f = 
     a.(i) <- f a.(i) in
   let pids = ref [] in
-  for i = 0 to (Array.length ar) - 1 do
-    pids := (Riot.spawn (fun () -> intermediate ar i func)) :: !pids
+  for i = 0 to Array.length ar - 1 do
+    pids := (Riot.spawn (fun () -> apply_func ar i func)) :: !pids
   done;
-  Riot.wait_pids !pids;;
+  let () = Riot.wait_pids !pids in
+  Array.to_list ar;;
 
-let multireduce ar sum =
-  let intermediate sum a =
-    sum := !sum + a in
+(* Parallel fold (sum) function *)
+let multifold ls =
+  let total_sum = ref 0 in
+  let accumulate a = 
+    total_sum := !total_sum + a in
   let pids = ref [] in
-  for i = 0 to (Array.length ar) - 1 do
-    pids := (Riot.spawn (fun () -> intermediate sum ar.(i))) :: !pids
+  List.iter (fun a ->
+    pids := (Riot.spawn (fun () -> accumulate a)) :: !pids
+  ) ls;
+  let () = Riot.wait_pids !pids in
+  !total_sum;;
+
+(* Parallel filter function *)
+let multifilter pred ls =
+  let ar = Array.of_list ls in
+  let results = Array.make (Array.length ar) false in
+  let apply_pred a i p = 
+    results.(i) <- p a.(i) in
+  let pids = ref [] in
+  for i = 0 to Array.length ar - 1 do
+    pids := (Riot.spawn (fun () -> apply_pred ar i pred)) :: !pids
   done;
-  Riot.wait_pids !pids;;
+  let () = Riot.wait_pids !pids in
+  (* Collect the results based on the results array *)
+  let filtered = ref [] in
+  for i = 0 to Array.length results - 1 do
+    if results.(i) then 
+      filtered := ar.(i) :: !filtered
+  done;
+  List.rev !filtered;;
 
-
+(* Main execution block *)
 Riot.run @@ fun () ->
-  let ar = [|1;2|] in
-  multimap ar addone;
-  let sum = ref 0 in
-  multireduce ar sum;
-  Format.printf "%d " !sum;
-  for i = 0 to (Array.length ar) - 1 do
-    Format.printf "%d " ar.(i);
-  done;
+  let ls = [1; 2; 3; 4; 5] in
+
+  (* Test multimap *)
+  let newls = multimap ls addone in
+  Format.printf "After multimap (addone): ";
+  List.iter (fun x -> Format.printf "%d " x) newls;
+  Format.printf "\n";
+
+  (* Test multifold *)
+  let sum = multifold ls in
+  Format.printf "Sum using multifold: %d\n" sum;
+
+  (* Test multifilter *)
+  let is_even x = x mod 2 = 0 in
+  let filtered_ls = multifilter is_even ls in
+  Format.printf "Filtered (even numbers): ";
+  List.iter (fun x -> Format.printf "%d " x) filtered_ls;
+  Format.printf "\n";
+
   Riot.shutdown ()
