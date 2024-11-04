c8f4c0cd0be04f806e42ab1126201657e20f205d
Dylan Bartell <barteldf@sunypoly.edu>
2024-11-04 14:51:32 -0500

    Added additional tree functions: find element and count elements

--- CS_431_Project_1/bin/main2.ml
+++ CS_431_Project_1/bin/main2.ml
@@ -1,11 +1,26 @@
 (* Project 2 - Multiprocessing Tree *)
 
-open Riot
+(* open Riot *)
 
 (* Defining a binary tree *)
 type tree =
   | Empty
-  | Node of int * tree * tree
+  | Node of int * tree * tree;;
+
+(* Get size of binary tree *)
+let _size t =
+  let sizepids = ref [] in
+    let count = ref 0 in
+    let rec helper t=
+      match t with
+      | Empty -> ()
+      | Node (_, l, r) -> count := (!count + 1);
+              sizepids := Riot.spawn (fun () -> helper l)::!sizepids;
+              sizepids := Riot.spawn (fun () -> helper r)::!sizepids in
+
+    helper t;
+    Riot.wait_pids !sizepids;
+    !count;;
 
 (* Function to insert an element into the binary tree *)
 let rec insert x = function
@@ -14,22 +29,40 @@ let rec insert x = function
       if x < v then
         Node(v, insert x left, right)
       else
-        Node(v, left, insert x right)
+        Node(v, left, insert x right);;
 
 (* Function to create a binary tree from a list of integers *)
 let tree_of_list lst =
     let tree = ref Empty in
-    let pids = ref [] in
+    let treelistpids = ref [] in
     List.iter (fun x ->
-      pids := (Riot.spawn (fun () -> tree := insert x !tree)) :: !pids
+      treelistpids := (Riot.spawn (fun () -> tree := insert x !tree)) :: !treelistpids
     ) lst;
-    Riot.wait_pids !pids;
-    !tree
+    Riot.wait_pids !treelistpids;
+    !tree;;
 
+(* Get boolean of whether element is in tree *)
+let find_element t x=
+  let findpids = ref [] in
+    let found = ref false in
+    let rec helper t=
+      match t with
+      | Empty -> ()
+      | Node (a, l, r) -> if a == x then found:= true;
+              findpids := Riot.spawn (fun () -> helper l)::!findpids;
+              findpids := Riot.spawn (fun () -> helper r)::!findpids in
 
+    helper t;
+    Riot.wait_pids !findpids;
+    !found;;
+  
+
+(* Main/run section *)
 let () =
     Riot.run @@ fun () ->
       let lst = [5; 3; 8; 1; 4; 7; 10] in
       let tree = tree_of_list lst in
-      (* You can add code here to print or manipulate the tree *)
-      Riot.shutdown ()
+      let t = insert 5 tree in
+      Format.print_bool (find_element t 5);
+      (*Format.printf "%d " (size t);*)
+      Riot.shutdown ();;
